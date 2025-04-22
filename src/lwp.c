50a193e8bd941c9187e978a2c3bbb06d20f8e777
David Voronov <davidvoronof@gmail.com>
2025-04-22 09:56:01 -0700

    add: lwp functions to implement skeleton

--- src/lwp.c
+++ src/lwp.c
@@ -1,2 +1,13 @@
 // light weight processes
 #include "../include/lwp.h"
+
+tid_t lwp_create(lwpfun func, void* arg) {}
+void lwp_exit(int status) {}
+tid_t lwp_gettid(void) {}
+void lwp_yield(void) {}
+void lwp_start(void) {}
+tid_t lwp_wait(int* status) {}
+void lwp_set_schedular(scheduler func) {}
+scheduler lwp_get_scheduler(void) {}
+thread tid2thread(tid_t tid) {}
+
